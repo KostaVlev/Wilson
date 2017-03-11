75d78f8704bf276eda6055ccf9b49fa44b1d4a95
KostaVlev <kostadinvlev@gmail.com>
2017-03-11 17:06:27 +0200

    Making Address to implement IValueObject.

--- Accounting/Wilson.Accounting.Core/Entities/Address.cs
+++ Accounting/Wilson.Accounting.Core/Entities/Address.cs
@@ -1,6 +1,9 @@
-﻿namespace Wilson.Accounting.Core.Entities
+﻿using System;
+using System.Reflection;
+
+namespace Wilson.Accounting.Core.Entities
 {
-    public class Address : Entity
+    public class Address : Entity, IValueObject<Address>
     {
         public string Country { get; set; }
 
@@ -17,5 +20,26 @@
         public string UnitNumber { get; set; }
 
         public string Note { get; set; }
+
+        public bool Equals(Address other)
+        {
+            if (this.Country.Equals(other.Country) && this.PostCode.Equals(other.PostCode) &&
+                this.Town.Equals(other.Town) && this.Street.Equals(other.Street) &&
+                this.StreetNumber.Equals(other.StreetNumber) && this.UnitNumber.Equals(other.UnitNumber))
+            {
+                if (this.Floor != null && this.Floor == other.Floor)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
